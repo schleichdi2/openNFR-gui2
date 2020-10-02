9a6e5f7789f14ce631c59c2a77749806a8257d77
schleichdi <schleichdi2@users.noreply.github.com>
2020-10-02 12:09:52 +0200

    Update LcdClockSelector.py

--- lib/python/Screens/LcdClockSelector.py
+++ lib/python/Screens/LcdClockSelector.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
 from Screens.Screen import Screen
 from Screens.Standby import TryQuitMainloop
 from Screens.MessageBox import MessageBox
